5dbea032e955109d1729a5cb0def66d4396a8ef2
wongchunhung <chunha@gmail.com>
2018-09-07 23:06:02 +0800

    add dna1.py

---
+++ dna1.py
@@ -0,0 +1,16 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+url = "https://198.18.129.100/api/system/v1/auth/token"
+
+headers = {'content-type': 'application/json'}
+
+resp = requests.post(url, auth=HTTPBasicAuth(username='admin', password='C1sco12345'), headers=headers,verify=False)
+
+print ("Request Status: ",resp.status_code)
+
+
+#token = resp.json()['Token']
+
+#print(token)
