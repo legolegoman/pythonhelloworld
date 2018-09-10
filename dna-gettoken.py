75ae75e1a610df004c437a98202edb1cb8f66f95
legolegoman <legolegoman@github.com>
2018-09-10 18:08:00 +0800

    add DNAC get token py

---
+++ dna-gettoken.py
@@ -0,0 +1,16 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+url = "https://10.1.5.19/api/system/v1/auth/token"
+
+headers = {'content-type': 'application/json'}
+
+resp = requests.post(url, auth=HTTPBasicAuth(username='admin', password='C1sc0123'), headers=headers,verify=False)
+
+print ("Request Status: ",resp.status_code)
+
+
+token = resp.json()['Token']
+
+print(token)
