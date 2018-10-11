9a19e1f5ae79a0f39a6e274896bcae940c11b626
wongchunhung <chunha@gmail.com>
2018-10-11 09:19:41 +0800

    Change to DNAC2.cisco.com

--- dna-gettoken.py
+++ dna-gettoken.py
@@ -2,11 +2,13 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-url = "https://10.1.5.19/api/system/v1/auth/token"
+#url = "https://10.1.5.19/api/system/v1/auth/token"
+url = "https://dnac2.cisco.com/api/system/v1/auth/token"
 
 headers = {'content-type': 'application/json'}
 
-resp = requests.post(url, auth=HTTPBasicAuth(username='admin', password='C1sc0123'), headers=headers,verify=False)
+#resp = requests.post(url, auth=HTTPBasicAuth(username='admin', password='C1sc0123'), headers=headers,verify=False)
+resp = requests.post(url, auth=HTTPBasicAuth(username='cisco', password='CiscoDNA!'), headers=headers,verify=False)
 
 print ("Request Status: ",resp.status_code)
 
--- dna1.py
+++ dna1.py
@@ -2,20 +2,24 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-url = "https://10.1.5.19/api/system/v1/auth/token"
+#url = "https://10.1.5.19/api/system/v1/auth/token"
+url = "https://dnac2.cisco.com/api/system/v1/auth/token"
+
 
 headers = {'content-type': 'application/json'}
 
-resp = requests.post(url, auth=HTTPBasicAuth(username='admin', password='C1sc0123'), headers=headers,verify=False)
+#resp = requests.post(url, auth=HTTPBasicAuth(username='admin', password='C1sc0123'), headers=headers,verify=False)
+resp = requests.post(url, auth=HTTPBasicAuth(username='cisco', password='CiscoDNA!'), headers=headers,verify=False)
 
-print ("Request Status: ",resp.status_code)
+#print ("Request Status: ",resp.status_code)
 
 
 token = resp.json()['Token']
 
 #print(token)
 
-url = "https://10.1.5.19/api/v1/network-device"
+url = "https://dnac2.cisco.com/api/v1/network-device"
+#url = "https://10.1.5.19/api/v1/network-device"
 
 headers = {'X-Auth-Token': token}
 
@@ -30,5 +34,6 @@ i = 0
 
 for _ in networkdevice['response']:
     networkdevice_tmp = networkdevice['response'][i]
-    print(networkdevice_tmp['platformId'],": ",networkdevice_tmp['hostname']," ",networkdevice_tmp['id'])
+#    print(networkdevice_tmp['platformId'],": ",networkdevice_tmp['hostname']," ",networkdevice_tmp['id'])
+    print(networkdevice_tmp['platformId'],": ",networkdevice_tmp['hostname'])
     i += 1
